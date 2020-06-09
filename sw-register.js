120aacc634e68f5fa21d3dacddb9de9d0432e213
Sumer Patel <sumerdpatel@gmail.com>
2020-06-09 15:18:59 +0530

    Update sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in Navigator){
   window.addEventListener('load', function () {
-    navigator.register('/sw.js').then((reg) => {
+    navigator.register('./sw.js').then((reg) => {
       console.log('Service worker registered successfully! ', reg.scope);
     }).catch((err) => {
       console.log('Service worker registration failed! ', err);
