3766ef5fcafea4b96bc6acf8bb830c585a1467b2
Sumer Patel <sumerdpatel@gmail.com>
2020-06-09 15:44:55 +0530

    changes to sw

--- sw-register.js
+++ sw-register.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then((reg) => {
+    navigator.serviceWorker.register('https://sumer16.github.io/relaxovat/sw.js').then((reg) => {
       console.log('Service worker registered successfully! ', reg.scope);
     }).catch((err) => {
       console.log('Service worker registration failed! ', err);
