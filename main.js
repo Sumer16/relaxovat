8da1aea07ffa52de7f2180a22ea3f64180d28c41
Sumer Patel <sumerdpatel@gmail.com>
2020-06-09 16:47:34 +0530

    added an alternate path

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ setInterval(breatheAnimation, totalTime);
 
 if('serviceWorker' in navigator){
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('./relaxovat/sw.js').then((reg) => {
+    navigator.serviceWorker.register('https://sumer16.github.io/relaxovat/sw.js').then((reg) => {
       console.log('Service worker registered successfully! ', reg.scope);
     }).catch((err) => {
       console.log('Service worker registration failed! ', err);
